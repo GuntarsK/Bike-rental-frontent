7d425205dff9bb73477baf980a81a5897d346a0a
Guntars Kirovainis <guntars.kirovainis@gmail.com>
2020-09-09 02:42:59 +0300

    Adding search function

--- app/bikelist/bikelist.js
+++ app/bikelist/bikelist.js
@@ -16,6 +16,28 @@ angular.module('myApp.bikelist', ['ngRoute'])
 
         $scope.searchClick = function ($) {
 
+            var bikeDto = {
+                manufacturer: $scope.manufacturer,
+                model: $scope.model,
+                category: $scope.category,
+                price: $scope.price,
+                pcs_in_stock: $scope.pcs_in_stock
+            }
+
+            var submitData = JSON.stringify(bikeDto, function (key, value) {
+                if (value === "") {
+                    return null;
+                }
+                return value;
+            })
+
+            $httpClient.post("http://127.0.0.1:8080/api/rest/bike.svc/bikes/search",submitData)
+                .then(function (response) {
+                    console.log(response);
+                    if (response.data.result != null && response.data.result === "SUCCESS") {
+                        $scope.bikeArray = response.data.holderList;
+                    }
+                })
         }
 
         $httpClient.get("http://127.0.0.1:8080/api/rest/bike.svc/bikes")
